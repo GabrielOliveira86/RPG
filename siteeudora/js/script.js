33cc20eaabc4cf0f3d0f713d66469d83cc360238
Gabriel <gabrieloliveirad@yahoo.com.br>
2021-10-06 11:26:44 -0300

    Teste Commit

---
+++ siteeudora/js/script.js
@@ -0,0 +1,23 @@
+function mostra(){
+	if(document.getElementById('cidades').style.display=='block'){
+
+		document.getElementById('cidades').style.display='none';
+	}
+	else{
+		document.getElementById('cidades').style.display='block'
+	}
+}
+
+/*
+var btn=document.queySelector('#show-or-hide');
+var container = document.queySelector('#cidades');
+btn.addEventListener('click', function(){
+
+	if(container.style.display==='block'){
+		container.style.display='none';
+	}else{
+		container.style.display='block';
+	}
+
+});
+*/

01f66dbd23becc3dbef9a05a8965330bdebc1fdd
Gabriel <gabrieloliveirad@yahoo.com.br>
2021-10-18 15:15:35 -0300

    Mudanças

--- siteeudora/js/script.js
+++ siteeudora/js/script.js
@@ -8,16 +8,4 @@ function mostra(){
 	}
 }
 
-/*
-var btn=document.queySelector('#show-or-hide');
-var container = document.queySelector('#cidades');
-btn.addEventListener('click', function(){
 
-	if(container.style.display==='block'){
-		container.style.display='none';
-	}else{
-		container.style.display='block';
-	}
-
-});
-*/
